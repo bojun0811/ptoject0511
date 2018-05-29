d8d6a96b25f9c043c99e1f92402fb5018afd0770
Bojun0811 <eva0905787440@gmail.com>
2018-05-29 18:43:51 +0800

    a non-constant pointer to constant data

--- main.c
+++ main.c
@@ -2,22 +2,22 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-void bubblesore(int a,int y);
+	void printcharacters(const char *sPtr);
 int main(int argc, char *argv[]) {
 
-void *y=&y;
-int x,a,tmp;
-int p;
-int *const ptr =&x;
-*ptr=7;
-//ptr =&y; 
-printf("ptr¦ì§} %d\n", &ptr);
-printf("y¦ì§} %d\n", &y);
-a=&y;
-tmp=&ptr;
-  y=tmp ;
-printf("y= %d\n", y);
- bubblesore( a,y );
+
+
+ char string[]="My homework!";
+puts("The string is:");
+printcharacters(string);
+puts("");
+return 0;
+}
+
+void printcharacters(const char *sPtr)
+{ 
+  for(;*sPtr!= '\0';++sPtr)
+  {  printf("%c",*sPtr);
+  }
 
 }
